098f13a2dfac88f3776da99f85f76eec0b0dd18d
pikachu <msram2006@gmail.com>
2024-12-25 17:04:25 +0530

    added middleware

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { listProducts, singleProduct, removeProduct, addProduct } from '../controllers/productController.js'
+import upload from '../middleware/multer.js'
 
 
 const productRouter = express.Router();
 
-productRouter.post('/add',addProduct);
+productRouter.post('/add',upload,addProduct);
 productRouter.post('/remove',removeProduct);
 productRouter.post('/single',singleProduct);
 productRouter.get('/list', listProducts);
